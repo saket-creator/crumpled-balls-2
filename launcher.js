a1441b22bd983ccd941fd5a64acd8ad087fd1950
saket-creator <65777399+saket-creator@users.noreply.github.com>
2020-07-11 16:21:03 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,45 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        
+        this.pointB = pointB
+        this.sling = Matter.Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    attach(body){
+        this.sling.bodyA = body;
+    }
+    
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            
+            stroke(48,22,8);
+          
+                strokeWeight(7);
+                line(pointA.x , pointA.y, pointB.x , pointB.y);
+                
+                //image(this.sling3,pointA.x -30, pointA.y -10,15,30);
+            
+            
+               
+            
+           
+            
+            pop();
+        }
+    }
+    
+}
